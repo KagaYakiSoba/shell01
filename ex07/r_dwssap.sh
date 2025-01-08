583f65fd70d711325fddef0f9468450da5e0ac2b
Ubuntu <ubuntu@ip-172-31-11-181.ap-northeast-1.compute.internal>
2025-01-08 02:57:20 +0000

    finished all exercises

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,1 +1,3 @@
 grep -v '^#' /etc/passwd | awk 'NR % 2 == 0' | cut -d: -f1 | awk -v start="$FT_LINE1" -v end="$FT_LINE2" 'NR >= start && NR <= end' | rev | sort -r | paste -sd, - | sed 's/, $/./'
+
+
